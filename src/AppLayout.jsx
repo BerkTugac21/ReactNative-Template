f9986b54bc0b23972db9878ba92484cd1cc5de6b
BerkTugac21 <berktugac@hotmail.com>
2024-05-25 19:10:53 +0300

    Expo 51 upgrade

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -2,10 +2,7 @@
 /* eslint-disable react/prop-types */
 import React, { useEffect } from 'react';
 import { Provider as StoreProvider } from 'react-redux';
-import {
-  initialWindowMetrics,
-  SafeAreaProvider,
-} from 'react-native-safe-area-context';
+
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 
@@ -21,21 +18,21 @@ export default function AppLayout(props) {
   const { hideSplashScreen } = props;
 
   useEffect(() => {
-    setTimeout(hideSplashScreen, 5000);
+    setTimeout(async () => {
+      await hideSplashScreen();
+    }, 3000);
   }, []);
 
   return (
     <ThemeProvider>
       <StoreProvider store={store}>
-        <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-          <GestureHandlerRootView style={{ flex: 1 }}>
-            <NotificationProvider>
-              <NavigationContainer>
-                <DrawerNavigator />
-              </NavigationContainer>
-            </NotificationProvider>
-          </GestureHandlerRootView>
-        </SafeAreaProvider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <NotificationProvider>
+            <NavigationContainer>
+              <DrawerNavigator />
+            </NavigationContainer>
+          </NotificationProvider>
+        </GestureHandlerRootView>
       </StoreProvider>
     </ThemeProvider>
   );
